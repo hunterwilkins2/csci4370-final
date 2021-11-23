31609f8cab91db7c3fde636276e1ef17f6ead940
Hunter Wilkins <hunter.wilkins2@gmail.com>
2021-11-23 10:57:13 -0500

    Finished homepage

--- index.php
+++ index.php
@@ -40,33 +40,84 @@
         </header>
 
         <main>
-            <h1>Space X</h1>
+            <div class="company-select">
+                <h1>Space X</h1>
+                <form method="get">
+                    <select name="company" id="company">
+                        <option value="spacex">Space X</option>
+                    </select>
+                </form>
+            </div>
             <div id="map"></div>
             <div id="info">
-                <table style="width: 100%; border-spacing: 0px;">
-                <table id="launch-table">
-                    <tr>
-                        <th>Pending Launch Date</th>
-                        <th>Latitude</th>
-                        <th>Longitude</th>
-                        <th>Color</th>
-                        <th>Display</th>
-                    </tr>
-                    <tr>
-                        <td>10/2/2022</td>
-                        <td>10</td>
-                        <td>30</td>
-                        <td class="map-label"><span class="site-color"></td>
-                        <td><input type="checkbox" checked>
-                    </tr>
-                    <tr>
-                        <td>5/30/2024</td>
-                        <td>37.09</td>
-                        <td>-40.92</td>
-                        <td class="map-label"><span class="site-color"></td>
-                        <td><input type="checkbox" checked>
-                    </tr>
-                </table>
+                <div>
+                    <h2>Satellites in orbit</h2>
+                    <table id="orbit-table">
+                        <tr>
+                            <th>Launch Date</th>
+                            <th>Launch Site Latitude</th>
+                            <th>Launch Site Longitude</th>
+                            <th>Altitude</th>
+                            <th>Inclination</th>
+                            <th>Color</th>
+                            <th>Display</th>
+                        </tr>
+                        <tr>
+                            <td>10/18/2021</td>
+                            <td>28.6272</td>
+                            <td>-80.6209</td>
+                            <td>408</td>
+                            <td>30.2</td>
+                            <td class="map-label"><span class="orbit-color"></td>
+                            <td><input type="checkbox" checked>
+                        </tr>
+                        <tr>
+                            <td>8/22/2020</td>
+                            <td>37.09</td>
+                            <td>-40.92</td>
+                            <td>602</td>
+                            <td>-70.3</td>
+                            <td class="map-label"><span class="orbit-color"></td>
+                            <td><input type="checkbox" checked>
+                        </tr>
+                        <tr>
+                            <td>2/9/2019</td>
+                            <td>34.5813</td>
+                            <td>-120.6266</td>
+                            <td>543</td>
+                            <td>50.1</td>
+                            <td class="map-label"><span class="orbit-color"></td>
+                            <td><input type="checkbox" checked>
+                        </tr>
+                    </table>
+                </div>
+
+                <div>
+                    <h2>Satellites waiting to be launched</h2>
+                    <table id="launch-table">
+                        <tr>
+                            <th>Pending Launch Date</th>
+                            <th>Latitude</th>
+                            <th>Longitude</th>
+                            <th>Color</th>
+                            <th>Display</th>
+                        </tr>
+                        <tr>
+                            <td>10/2/2022</td>
+                            <td>28.6272</td>
+                            <td>-80.6209</td>
+                            <td class="map-label"><span class="site-color"></td>
+                            <td><input type="checkbox" checked>
+                        </tr>
+                        <tr>
+                            <td>5/30/2024</td>
+                            <td>34.5813</td>
+                            <td>-120.6266</td>
+                            <td class="map-label"><span class="site-color"></td>
+                            <td><input type="checkbox" checked>
+                        </tr>
+                    </table>
+                </div>
             </div>
         </main>
 
--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,47 +1,190 @@
-let matDiv;
-let launchTable;
+let orbitArr;
+let launchArr;
 let myMap;
 let canvas;
-const mappa = new Mappa('Leaflet');
+let img;
 
-// Lets put all our map options in a single object
+const key = 'pk.eyJ1IjoiZ2FsYXh5ODIiLCJhIjoiY2tkZ2o3aDV0Mjh6MDMwcGd0dGNkemxwNCJ9.4Dhyt_1dEpmgG4UKnS-WQg';
+const mappa = new Mappa('MapboxGL', key);
 const options = {
-  lat: 0,
+  lat: 10,
   lng: 0,
-  zoom: 4,
-  style: "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+  zoom: 1.2,
+  minZoom: 0.8,
+  renderWorldCopies: false,
+  style: "mapbox://styles/mapbox/satellite-streets-v10"
+}
+
+
+function preload() {
+    img = loadImage('./satellite.png');
 }
 
 function setup(){
-    launchTable = document.getElementById('launch-table');
-    setLabelColors(launchTable);
-
-    mapDiv = document.getElementById('map'); 
-    canvas = createCanvas(mapDiv.offsetWidth, mapDiv.offsetWidth / 3); 
+    let mapDiv = document.getElementById('map'); 
+    canvas = createCanvas(mapDiv.offsetWidth, mapDiv.offsetHeight); 
     canvas.parent('map');
+    imageMode(CENTER);
+    angleMode(DEGREES);
 
     // Create a tile map with the options declared
     myMap = mappa.tileMap(options); 
     myMap.overlay(canvas);
+
+    let orbitTable = document.getElementById('orbit-table');
+    let launchTable = document.getElementById('launch-table');
+
+    orbitArr = parseTableToObject(orbitTable);
+    setLabelColors(orbitArr);
+    cacheGroundTrace(orbitArr);
+
+    launchArr = parseTableToObject(launchTable);
+    setLabelColors(launchArr);
+
+    frameRate(10);
 }
 
 function draw(){
     clear();
     drawLaunchSites();
+    drawOrbits();
+}
+
+function drawOrbits() {
+    for (let index = 0; index < orbitArr.length; index++) {
+        if(orbitArr[index].Display.checked) {
+            let groundTrack = orbitArr[index].GroundTrack;
+
+            stroke(orbitArr[index].Color.style.backgroundColor);
+            strokeWeight(3);
+            noFill();
+            for (let i = 0; i < groundTrack.length; i++) {
+                if (i < groundTrack.length - 1 && groundTrack[i].y < groundTrack[i + 1].y) {
+                    const p1 = myMap.latLngToPixel(groundTrack[i].x, groundTrack[i].y);
+                    const p2 = myMap.latLngToPixel(groundTrack[i + 1].x, groundTrack[i + 1].y);
+                    line(p1.x, p1.y, p2.x, p2.y);
+                }
+            }
+
+            const currentLocation = getCurrentLocation(
+                new Date(orbitArr[index]['Launch Date']),
+                parseFloat(orbitArr[index]['Launch Site Latitude']), 
+                parseFloat(orbitArr[index]['Launch Site Longitude']), 
+                parseFloat(orbitArr[index].Altitude), 
+                parseFloat(orbitArr[index].Inclination));      
+                
+                const latLong = myMap.latLngToPixel(currentLocation.x, currentLocation.y);
+                const zoom = myMap.zoom() + 1;
+                image(img, latLong.x, latLong.y, 15 * zoom, 15 * zoom);  
+        }
+    }
 }
 
 function drawLaunchSites() {
-    var rowLength = launchTable.rows.length;
+    stroke(0);
+    strokeWeight(1);
+    for (let index = 0; index < launchArr.length; index++) {
+        if(launchArr[index].Display.checked) {
+            const site = myMap.latLngToPixel(launchArr[index].Latitude, launchArr[index].Longitude);
+            fill(launchArr[index].Color.style.backgroundColor);
+            ellipse(site.x, site.y, 15, 15);
+        }
+    }
+}
 
-    for (i = 1; i < rowLength; i++){
-        var cells = launchTable.rows.item(i).cells;
-        if(cells.item(4).children[0].checked) {
-            const site = myMap.latLngToPixel(cells.item(1).innerHTML, cells.item(2).innerHTML); 
-            fill(cells.item(3).children[0].style.backgroundColor)
-            ellipse(site.x, site.y, 20, 20);        
+function cacheGroundTrace(arr) {
+    for (let index = 0; index < arr.length; index++) {
+        let currentOrbit = arr[index];
+
+        currentOrbit['GroundTrack'] = getGroundTrack(
+            new Date(currentOrbit['Launch Date']),
+            parseFloat(currentOrbit['Launch Site Latitude']), 
+            parseFloat(currentOrbit['Launch Site Longitude']), 
+            parseFloat(currentOrbit.Altitude), 
+            parseFloat(currentOrbit.Inclination));        
+    }
+}
+
+const orbits = 3; // Number of orbits to calculate
+const timeStep = 180; // Amount of steps to calculate per total degrees
+const degreesPerSecond = 360 / 86400; // The amount of degrees the earth rotates per second
+const R = 6378.1; // Radius of earth
+
+function getGroundTrack(epoch, latitude, longitude, altitude, inclination) {
+    const timeSinceLaunch = (new Date() - epoch) / 1000;
+    const T = sqrt(pow((altitude + R) * 1000, 3) * 9.907 * pow(10, -14));
+    let phase = asin(latitude / inclination);
+    const deltaTime = orbits * T / timeStep;
+    const earthRotationPerOrbit = T * degreesPerSecond;
+    const deltaRotation = earthRotationPerOrbit / timeStep;
+
+    let groundTrack = [];
+    let long;
+    for (let t = 0; t < orbits * T; t += deltaTime) {
+        let lat = inclination * sin((360 / T) * (t + timeSinceLaunch) + phase);
+        long = (360 / T) * (t + timeSinceLaunch) + longitude;
+        long = long - (360 * floor(long / 360))
+        if(long > 180) {
+            long -= 360;
         }
 
+        groundTrack.push(createVector(lat, long));
+
+        phase += deltaRotation;
     }
+
+    return groundTrack;
+}
+
+function getCurrentLocation(epoch, latitude, longitude, altitude, inclination) {
+    const timeSinceLaunch = (new Date() - epoch) / 1000;
+    const T = sqrt(pow((altitude + R) * 1000, 3) * 9.907 * pow(10, -14));
+    let phase = asin(latitude / inclination);
+
+    let lat = inclination * sin((360 / T) * timeSinceLaunch + (phase));
+    long = (360 / T) * timeSinceLaunch + longitude;
+    long = long - (360 * floor(long / 360))
+    if(long > 180) {
+        long -= 360;
+    }
+
+    return createVector(lat, long);
+}
+
+function parseTableToObject(table) {
+    var data = [];
+
+    var rowLength = table.rows.length;
+
+    // Create parameters
+    var params = [];
+    if(rowLength >= 1) {
+        var cells = table.rows.item(0).cells;
+        for(param = 0; param < cells.length; param++) {
+            params.push(cells.item(param).innerHTML);
+        }
+    } else {
+        return [];
+    }
+
+    for (i = 1; i < rowLength; i++){
+        var cells = table.rows.item(i).cells;
+
+        var cellLength = cells.length;
+
+        let column = {};
+        for(var j = 0; j < cellLength; j++){
+            if(cells.item(j).children.length > 0) {
+                column[params[j]] = cells.item(j).children[0];
+            } else {
+                column[params[j]] = cells.item(j).innerHTML;
+            }
+        }
+
+        data.push(column);
+    }
+
+    return data;
 }
 
 function getColor(current, length) {
@@ -49,18 +192,8 @@ function getColor(current, length) {
     return `hsl(${map}, 100%, 70%)`;
 }
 
-function setLabelColors(table) {
-    var rowLength = table.rows.length;
-
-    for (i = 0; i < rowLength; i++){
-        var cells = table.rows.item(i).cells;
-
-        var cellLength = cells.length;
-
-        for(var j = 0; j < cellLength; j++){
-            if(cells.item(j).classList.contains('map-label')) {
-                cells.item(j).children[0].style.backgroundColor = getColor(i, rowLength);
-            }
-        }
+function setLabelColors(arr) {
+    for (let index = 0; index < arr.length; index++) {
+        arr[index].Color.style.backgroundColor = getColor(index, arr.length  + 1) 
     }
 }
