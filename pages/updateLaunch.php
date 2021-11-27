7a25e393ffb5f74ffd6a51520ba2b2fa436d9a59
Annika Hultin <amh14776@uga.edu>
2021-11-27 13:47:45 -0500

    added update pages

---
+++ pages/updateLaunch.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang = "en">
+
+<body>
+<form action="tut1.php" method="get">
+
+
+<div>
+<h2>Update Pending Satelite Launch</h2>
+
+<div/>
+<div>
+    <label>Satelite Name:</label>
+     <input>
+</div>
+<div>
+    <label>Launch Location:</label>
+     <input>
+</div>
+<div>
+    <label>Pending Launch Date:</label>
+     <input type="date">
+</div>
+
+
+     <div>
+    <button>Submit</button>
+</div>
+    </form>
+
+
+</body>
+</html>
---
+++ pages/updateLocation.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang = "en">
+
+<body>
+<form action="tut1.php" method="get">
+
+
+<div>
+<h2>Update Satelite Location</h2>
+
+<div/>
+<div>  
+<label>Satelite ID : </label>
+<input>
+</div>
+
+<div>
+    <label>Satelite Current Location :</label>
+     <input>
+</div>
+<div>
+    <label>Satelite New Location:</label>
+     <input>
+</div>
+
+<div>
+    <label>Satelite Name:</label>
+     <input>
+</div>
+
+
+     <div>
+    <button>Submit</button>
+</div>
+    </form>
+
+
+</body>
+</html>
