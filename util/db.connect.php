fdd06a29e060cb884b1a6df9e17a9549ebbfc836
Hunter Wilkins <hunter.wilkins2@gmail.com>
2021-12-02 17:18:12 -0500

    Added port to .env.example file

--- util/db.connect.php
+++ util/db.connect.php
@@ -3,7 +3,7 @@
 
     (new DotEnv(__DIR__ . '/../.env'))->load();
 
-    $mysqli = new mysqli(getenv("HOST"), getenv("USER"), getenv("PASSWORD"), getenv("DATABASE"));
+    $mysqli = new mysqli(getenv("HOST"), getenv("USER"), getenv("PASSWORD"), getenv("DATABASE"), getenv("PORT"));
 
     // Check connection
     if ($mysqli -> connect_errno) {
