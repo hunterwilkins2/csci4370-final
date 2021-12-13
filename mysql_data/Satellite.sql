444dc3b66a4038ae3fbdbdc5c428ca6e45ed7b54
Hunter Wilkins <hunter.wilkins2@gmail.com>
2021-12-13 16:39:38 -0500

    Added real companies to mysql data

--- mysql_data/Satellite.sql
+++ mysql_data/Satellite.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 01, 2021 at 06:00 PM
+-- Generation Time: Dec 13, 2021 at 04:38 PM
 -- Server version: 8.0.27-0ubuntu0.20.04.1
 -- PHP Version: 7.4.3
 
@@ -12,78 +12,15 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Database: `Satellite`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Companies`
---
-
-DROP TABLE IF EXISTS `Companies`;
-CREATE TABLE `Companies` (
-  `company_id` int UNSIGNED NOT NULL,
-  `company_name` varchar(20) NOT NULL,
-  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
---
--- Dumping data for table `Companies`
---
-
-INSERT INTO `Companies` (`company_id`, `company_name`, `password`, `address`) VALUES
-(1, 'Yamin\'s Test Company', '5f4dcc3b5aa765d61d8327deb882cf99', 'Test'),
-(5, 'Test company', '$2y$10$zdbuRXDqnyBwwxYI8emXVussMuwLRkMouwa4erJDY5gActjYaS3hK', '123 Apple Street');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `In-Orbit`
---
-
-DROP TABLE IF EXISTS `In-Orbit`;
-CREATE TABLE `In-Orbit` (
-  `oid` int NOT NULL,
-  `satellite_id` int UNSIGNED NOT NULL,
-  `launch_date` date NOT NULL,
-  `launch_latitude` float NOT NULL,
-  `launch_longitude` float NOT NULL,
-  `altitude` float NOT NULL,
-  `inclination` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
---
--- Dumping data for table `In-Orbit`
---
-
-INSERT INTO `In-Orbit` (`oid`, `satellite_id`, `launch_date`, `launch_latitude`, `launch_longitude`, `altitude`, `inclination`) VALUES
-(0, 2, '2019-04-04', 0, 0, 0, 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Pending`
---
-
-DROP TABLE IF EXISTS `Pending`;
-CREATE TABLE `Pending` (
-  `pid` int NOT NULL,
-  `satellite_id` int UNSIGNED NOT NULL,
-  `pending_date` date NOT NULL,
-  `pending_latitude` float NOT NULL,
-  `pending_longitude` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
---
--- Dumping data for table `Pending`
---
-
-INSERT INTO `Pending` (`pid`, `satellite_id`, `pending_date`, `pending_latitude`, `pending_longitude`) VALUES
-(1, 1, '2023-03-04', 0, 0),
-(2, 1, '2023-03-04', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -105,33 +42,26 @@ CREATE TABLE `Satellites` (
 --
 
 INSERT INTO `Satellites` (`satellite_id`, `company_id`, `satellite_name`, `model`, `type`) VALUES
-(1, 1, 'Cool Test Satelite', '3213z4', 'Pending'),
-(2, 1, 'MOCI', 'tz3024', 'In-Orbit');
+(14, 6, 'StarLink 84', 'TinTin', 'Pending'),
+(15, 6, 'Transporter 2', 'Falcon 9', 'Pending'),
+(16, 6, 'Inspiration4', 'Falcon Heavy', 'Pending'),
+(17, 6, 'Crew 6', 'Falcon Heavy', 'Pending'),
+(18, 6, 'StarLink 34', 'TinTin', 'In-Orbit'),
+(19, 6, 'StarLink 42', 'V1.0 with Lazers', 'In-Orbit'),
+(21, 6, 'Dreamer', 'Falcon 9', 'In-Orbit'),
+(22, 7, 'Orion', 'Thor Delta', 'Pending'),
+(23, 7, 'USA 193', 'Vanguard', 'Pending'),
+(24, 7, 'ISS', 'Zvezda', 'In-Orbit'),
+(25, 7, 'Hubble Telescope', 'Lockhead Perkin', 'In-Orbit'),
+(26, 8, 'Sputnik 1', 'OKB 1', 'In-Orbit'),
+(27, 8, 'Soyuz', 'Energia', 'In-Orbit'),
+(28, 8, 'Kosmos 2549', 'Soyuz 2 1b', 'Pending'),
+(29, 8, 'Arktika M 1', 'Fregat', 'Pending'),
+(30, 8, 'Ekspress AMu3', 'Proton M', 'Pending');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `Companies`
---
-ALTER TABLE `Companies`
-  ADD PRIMARY KEY (`company_id`),
-  ADD UNIQUE KEY `company_name` (`company_name`);
-
---
--- Indexes for table `In-Orbit`
---
-ALTER TABLE `In-Orbit`
-  ADD PRIMARY KEY (`oid`),
-  ADD KEY `satellite_id` (`satellite_id`) USING BTREE;
-
---
--- Indexes for table `Pending`
---
-ALTER TABLE `Pending`
-  ADD PRIMARY KEY (`pid`),
-  ADD KEY `satellite_id` (`satellite_id`) USING BTREE;
 
 --
 -- Indexes for table `Satellites`
@@ -145,38 +75,14 @@ ALTER TABLE `Satellites`
 --
 
 --
--- AUTO_INCREMENT for table `Companies`
---
-ALTER TABLE `Companies`
-  MODIFY `company_id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `Pending`
---
-ALTER TABLE `Pending`
-  MODIFY `pid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
 -- AUTO_INCREMENT for table `Satellites`
 --
 ALTER TABLE `Satellites`
-  MODIFY `satellite_id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `satellite_id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `In-Orbit`
---
-ALTER TABLE `In-Orbit`
-  ADD CONSTRAINT `in-orbit satellite_ibfk_1` FOREIGN KEY (`satellite_id`) REFERENCES `Satellites` (`satellite_id`);
-
---
--- Constraints for table `Pending`
---
-ALTER TABLE `Pending`
-  ADD CONSTRAINT `pending launch satellite_ibfk_1` FOREIGN KEY (`satellite_id`) REFERENCES `Satellites` (`satellite_id`);
 
 --
 -- Constraints for table `Satellites`
@@ -184,3 +90,7 @@ ALTER TABLE `Pending`
 ALTER TABLE `Satellites`
   ADD CONSTRAINT `Satellites_ibfk_1` FOREIGN KEY (`company_id`) REFERENCES `Companies` (`company_id`);
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
